550dbf5a6825ad7f97ed217ed85b6ca52f4afc8f
Álvaro Sobrino <alvaro.sobrino@mtp.es>
2024-05-14 16:55:33 +0200

    commit api base

---
+++ BACK/document_api.py
@@ -0,0 +1,19 @@
+﻿from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI() # Objeto con el que manejaremos la API
+
+# Primero haz: cd .\BACK\
+# uvicorn document_api:app --reload
+
+class Item(BaseModel):
+	name: str
+
+@app.get( "/get/" )
+async def root ( text : str ) -> dict:
+	return { "text" : text }
+
+
+@app.post( "/post/" )
+async def root ( item : Item ) -> dict:
+	return { "item" : item.name }
