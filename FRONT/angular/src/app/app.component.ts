b9f9d6c00be9b942f7150c5ba6a3ec878755a088
m0usedev <alvaro.sobrino.coding@gmail.com>
2024-05-20 19:31:15 +0200

    app component modification

--- FRONT/angular/src/app/app.component.ts
+++ FRONT/angular/src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { API_URL_GET, API_URL_POST } from '../utilities/api';
+
+import { FormSendCsvComponent } from './form-send-csv/form-send-csv.component';
+import { GetDataCsvComponent } from './get-data-csv/get-data-csv.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormSendCsvComponent,
+    GetDataCsvComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular';
+
+  async fetchGet ( texto : string) {
+    fetch( API_URL_GET+texto )
+      .then( (response) => response.json() )
+      .then( (data) => console.log(data) )
+  }
+
+  async fetchPost ( texto : string) {
+    var data = {"name": texto}
+
+    fetch(
+      API_URL_POST,
+      {
+        method: "POST", // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then( (response) => response.json() )
+    .then( (data) => console.log(data) )
+  }
+
 }
