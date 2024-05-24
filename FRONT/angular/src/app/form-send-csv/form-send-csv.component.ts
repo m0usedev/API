d7ec15e653ee828ca8978090454809acaccb8d0f
m0usedev <alvaro.sobrino.coding@gmail.com>
2024-05-24 14:44:06 +0200

    changes front

--- FRONT/angular/src/app/form-send-csv/form-send-csv.component.ts
+++ FRONT/angular/src/app/form-send-csv/form-send-csv.component.ts
@@ -9,25 +9,83 @@ import { API_URL_UPLOAD_POST_CSV } from '../../utilities/api';
   styleUrl: './form-send-csv.component.css'
 })
 export class FormSendCsvComponent {
+  state = 'select'
+
+  stateUploadButton (state : string | any = undefined, response : object | string | any = undefined ) {
+    /**
+     * select - estado normal
+     * upload - subiendose el archivo
+     * error - cuando no se suvio el archivo, error en le servidor o
+     */
+    /**
+     * respuestas:
+     *  'bad', 'El fichero fue subido al servidor no es tipo .csv'
+     *  'good', 'El fichero fue subido al servidor'
+     * 'error', 'Ha surgido un error en el servidor'
+     */
+    if(response && response.response){
+      let tipo = response.response
+      switch (tipo) {
+        case 'bad':
+          this.state = 'error'
+          alert(response.message)
+          this.state = 'select'
+          break;
+        case 'good':
+          this.state = 'select'
+          alert(response.message)
+          break;
+        case 'error':
+          this.state = 'error'
+          alert(response.message)
+          this.state = 'select'
+          break;
+        default:
+          this.state = 'select'
+          break;
+      }
+    }else{
+      switch (state) {
+        case 'select':
+          this.state = 'select'
+          alert(response)
+          break;
+        case 'upload':
+          this.state = 'upload'
+          break;
+        case 'error':
+          this.state = 'error'
+          alert(response)
+          this.state = 'select'
+          break;
+        default:
+          this.state = 'select'
+          break;
+      }
+    }
+  }
 
   async onSubmit(event: Event) {
     const input = event.target as HTMLInputElement;
-
     if (input.files && input.files.length > 0) {
       if(input.files[0].type == 'text/csv'){
         const formData = new FormData();
         formData.append('file', input.files[0]);
         await this.fetchPostFile(formData);
+        input.value = ''
       }else{
-        console.log('El fichero debe ser de tipo csv')
+        input.value = ''
+        this.stateUploadButton('error', 'El fichero debe ser de tipo csv')
       }
     } else {
       console.error('No has seleccionado ningun fichero');
+      input.value = ''
+      this.stateUploadButton('select', 'No has seleccionado ningun fichero')
     }
   }
 
-
   async fetchPostFile ( form : FormData ) {
+    this.stateUploadButton('upload', undefined)
     fetch(
       API_URL_UPLOAD_POST_CSV,
       {
@@ -36,6 +94,10 @@ export class FormSendCsvComponent {
       }
     )
     .then( (response) => response.json() )
-    .then( (data) => console.log(data) )
+    .then( (data) => {
+      if(data){
+        this.stateUploadButton(undefined, data)
+      }
+    } )
   }
 }
--- FRONT/angular/src/app/get-profiling/get-profiling.component.ts
+++ FRONT/angular/src/app/get-profiling/get-profiling.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { API_URL_DOWNLOAD_GET_PROFILE } from '../../utilities/api';
-
+import { Component } from '@angular/core'
+import { API_URL_DOWNLOAD_GET_PROFILE } from '../../utilities/api'
+interface User {
+  "file"      : Blob,
+  "file_name" : string
+}
 @Component({
   selector: 'app-get-profiling',
   standalone: true,
@@ -9,45 +12,82 @@ import { API_URL_DOWNLOAD_GET_PROFILE } from '../../utilities/api';
   styleUrl: './get-profiling.component.css'
 })
 export class GetProfilingComponent {
-  file_get_data = ''
-  data : any
-  colums_key : any
+  file_get_data  : string
+  visible        : false | true
+  stateProfiling : false | true
+  file_download  : User
 
-  responseData (blob : any, file : string) {
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = url;
-    a.download = `${file}.html`;
-    document.body.appendChild(a);
-    a.click();
-    window.URL.revokeObjectURL(url);
+  constructor() {
+    this.file_get_data  = ''
+    this.visible        = false
+    this.stateProfiling = false
+    this.file_download  = {
+      "file"      : new Blob(),
+      "file_name" : ''
+    }
   }
 
-  async fetchGetData(file: string) {
+  renderHtml(blob: Blob) {
+    const reader = new FileReader()
+    reader.onload = (event) => {
+        const contentFrame = document.getElementById('contentFrame') as HTMLIFrameElement;
+        if (contentFrame && contentFrame.contentDocument && event.target) {
+            const result = event.target.result;
+            if (typeof result === 'string') {
+                const doc = contentFrame.contentDocument;
+                doc.open();
+                doc.write(result);
+                doc.close();
+                this.visible = true
+                this.stateProfiling = false
+            }
+        }
+    };
+    reader.readAsText(blob);
+  }
+
+  downbload() {
+    let blob = this.file_download.file
+    let file_name = this.file_download.file_name
+    const url = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.style.display = 'none'
+    a.href = url
+    a.download = `${file_name}.html`
+    document.body.appendChild(a)
+    a.click()
+    window.URL.revokeObjectURL(url)
+  }
+
+  async fetchGetData(file_name: string) {
+    this.visible = false
+    this.stateProfiling = true
     try {
-      const response = await fetch(API_URL_DOWNLOAD_GET_PROFILE + '?file=' + file, {
+      const response = await fetch(API_URL_DOWNLOAD_GET_PROFILE + '?file_name=' + file_name, {
         method: "GET"
-      });
+      })
 
       if (response.ok) {
-        const contentType = response.headers.get('content-type');
+        const contentType = response.headers.get('content-type')
 
         if (contentType && contentType.includes('application/json')) {
-          const jsonData = await response.json();
-          console.log('Error:', jsonData);
-          alert(jsonData.error);
+          const jsonData = await response.json()
+          if(jsonData.response )
+            this.stateProfiling = false
+            alert(jsonData.message)
         } else {
-          const blob = await response.blob();
-          this.responseData(blob, file);
+          const blob = await response.blob()
+          this.renderHtml(blob)
+          this.file_download.file = blob
+          this.file_download.file_name = file_name
         }
       } else {
-        console.error('Respuesta no ok:', response);
-        alert('Error al descargar el archivo');
+        console.error('Respuesta no ok:', response)
+        alert('Error al descargar el archivo')
       }
     } catch (error) {
-      console.error('Error:', error);
-      alert('Hubo un problema con la solicitud');
+      console.error('Error:', error)
+      alert('Hubo un problema con la solicitud')
     }
   }
 
